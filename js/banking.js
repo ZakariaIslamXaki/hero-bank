784a408d8c05b185b3f2dea0c79717124607dc63
ZakariaIslamXaki <zaki2995@gmail.comm>
2022-02-16 00:14:07 +0600

    balance section updated

--- js/banking.js
+++ js/banking.js
@@ -2,13 +2,21 @@ document.getElementById('deposit-button').addEventListener('click', function() {
     const depositInput = document.getElementById('deposit-input');
     const newDepositAmountText = depositInput.value;
     const newDepositAmount = parseFloat(newDepositAmountText);
+    // update deposit total
     const depositTotal = document.getElementById('deposit-total');
     const previousDepositText = depositTotal.innerText;
     const previousDepositAmount = parseFloat(previousDepositText)
     const newDepositTotal = previousDepositAmount + newDepositAmount;
     
-   
     depositTotal.innerText = newDepositTotal;
 
+    // update deposit total
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotal.innerText = newBalanceTotal;
+
+    // clear the deposit input field
     depositInput.value = '';
 })
